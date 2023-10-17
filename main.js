fb6c6b08049969b4452ea80109fad236858630e4
julia <126582644+liarivaille@users.noreply.github.com>
2023-10-17 07:56:26 -0300

    ok

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+//criando/declarando a função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
+//chamando/invocando a função tocaSomPom
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap
-
+//criando a referência constanate listaDeTeclas 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//atribuindo o clique no botão "0" que é o 1º item da lista de teclas 
 listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+//criando o código de repetição WHILE
+while () {
+
+}
