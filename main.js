99c7efd682a7c3d400af39b74f71e178c5570689
julia <126582644+liarivaille@users.noreply.github.com>
2023-10-24 07:51:54 -0300

    ok

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 //chamando/invocando a função tocaSom com parâmetro idElementoAudio
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSom;
 
 //criando a referência constanate listaDeTeclas 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //atribuindo o clique no botão "0" que é o 1º item da lista de teclas 
-listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[0].onclick = tocaSom;
 //criando referência variável contador, atribuindo o valor inicial como "0"
 let contador = 0;
 //criando o código de repetição WHILE
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log (contador);
 }
