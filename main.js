f1699b44cbcf75b01e9d36cc4fd4e6f7ff9cd662
julia <126582644+liarivaille@users.noreply.github.com>
2023-11-21 07:49:16 -0300

    ok

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-//criando/declarando a função tocaSom com parâmetro idElementoAudio 
+//criando/declarando a função tocaSom com parâmetro idElementoAudio
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-//criando a referência constante listaDeTeclas 
+//criando a referência constante listaDeTeclas
 const listaDeTeclas = document.querySelectorAll('.tecla');
-// criando refêrencia variável contador, atribuindo o valor inicial com "0"
-//criando o código de repetição WHILE
-//enquanto o contador for menos que o comprimento da lista, tocará o som
+//Criando o código de repetição WHILE e mudando para FOR
+//enquanto o contador < comprimento da lista, tocará o som.
 for(let contador = 0;contador < listaDeTeclas.length;contador++){
     const tecla = listaDeTeclas[contador];
-    const efeito [contador] .classList[1];//chama a classe do som
-    const idAudio = #som_§efeito;//junção com template string
+    const efeito = tecla.classList[1]; //chama a classe do indice 1 do som
+    const idAudio = `#som_${efeito}`; //junção com template string
     tecla.onclick = function(){ //função anônima para tocaSom
         tocaSom(idAudio);
-
     }
+    tecla.onkeydown = function(evento){ //função para tecla pressionada
+        if(evento.code != 'Tab'){
+            tecla.classList.add('ativa');
+        }
+    } 
+    tecla.onkeyup = function(){ //função para tecla despressionada
+        tecla.classList.remove('ativa');
+    }
+}
